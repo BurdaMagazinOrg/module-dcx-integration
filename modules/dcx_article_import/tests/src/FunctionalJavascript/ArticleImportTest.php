a0b53b8dd9025c2a7975025c440c2eff4ad6118e
Christian Fritsch <chr.fritsch@gmx.net>
2018-04-18 09:29:14 +0200

    Add test module

--- modules/dcx_article_import/tests/src/FunctionalJavascript/ArticleImportTest.php
+++ modules/dcx_article_import/tests/src/FunctionalJavascript/ArticleImportTest.php
@@ -4,6 +4,9 @@ namespace Drupal\Tests\dcx_article_import\FunctionalJavascript;
 
 use Drupal\FunctionalJavascriptTests\JavascriptTestBase;
 
+/**
+ * Class ArticleImportTest.
+ */
 class ArticleImportTest extends JavascriptTestBase {
 
   /**
@@ -12,8 +15,8 @@ class ArticleImportTest extends JavascriptTestBase {
    * @var array
    */
   public static $modules = [
-
     'dcx_article_import',
+    'dcx_test',
   ];
 
   /**
@@ -21,7 +24,16 @@ class ArticleImportTest extends JavascriptTestBase {
    */
   public function testArticleImport() {
 
+    $jsonclientsettings = json_decode(getenv('DCX_SETTINGS'), 1);
+    $this->config('dcx_integration.jsonclientsettings')->setData($jsonclientsettings)->save();
+    $this->config('system.site')->setData(['mail' => 'admin@admin.de', 'name' => 'Integration Test'])->save();
+
+    $this->drupalLogin($this->createUser(['import from dcx']));
+
     $this->drupalGet('node/add/article/dcx-import');
+
+#    $this->getSession()->getPage()->fillField()
+
   }
 
 }
